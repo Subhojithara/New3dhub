446aaf4134c741a2f10484b8d39a333d1cc08616
subhajit hara <harasubhojit@gmail.com>
2024-07-31 20:03:59 +0530

    changes

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -4,12 +4,14 @@ import MoreBorder from "@/components/Hero/MoreBorder";
 import NavbarDemo from "@/components/Navbar";
 
 import Image from "next/image";
+import LogoCarousel from "@/components/Hero/LogoCarousel";
 
 export default function Home() {
   return (
     <div className="bg-white text-black">
       <NavbarDemo />
       <HeroSection />
+      <LogoCarousel />
       <MoreBorder />
       <Ripple />
     </div>
--- src/components/Hero/LogoCarousel.tsx
+++ src/components/Hero/LogoCarousel.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+"use client";
+import Image from 'next/image';
 
 const LogoCarousel = () => {
-  return (
-    <div>LogoCarousel</div>
-  )
-}
 
-export default LogoCarousel
+
+    return (
+        <div className="relative h-40 flex flex-col justify-center overflow-hidden">
+            <div className="w-full max-w-5xl mx-auto px-4 md:px-6 py-24">
+                <div className="text-center">
+                    <div className="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-128px),transparent_100%)]">
+                        <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll">
+                            <li><Image className="h-10 w-auto" src="/icons/boat.png" alt="boat" width={100} height={40} /></li>
+                            <li><Image className="h-10 w-auto" src="/icons/audi.png" alt="Audi" width={100} height={40} /></li>
+                            <li><Image className="h-24 w-auto" src="/icons/Disney+ Hotstar.png" alt="Disney+ Hotstar" width={240} height={40} /></li>
+                            <li><Image className="h-20 w-auto" src="/icons/RoyalEnfield.png" alt="RoyalEnfield" width={200} height={40} /></li>
+                            <li><Image className="h-10 w-auto" src="/icons/airbnb.png" alt="Airbnb" width={100} height={40} /></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default LogoCarousel;
