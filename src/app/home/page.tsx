949b7b239a5e11aba6a497eb13fc50048ebc996a
subhajit hara <harasubhojit@gmail.com>
2024-08-02 13:28:28 +0530

    changes

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,27 +1,9 @@
-import HeroSection from "@/components/Hero/HeroSection";
-import Ripple from "@/components/Hero/Ripple";
-import MoreBorder from "@/components/Hero/MoreBorder";
-import LogoCarousel from "@/components/Hero/LogoCarousel";
-import ValueProp from "@/components/Hero/ValueProp";
-import OurProses from "@/components/Hero/OurProses";
-import Footer from "@/components/Footer";
-import MarketSize from "@/components/Hero/MarketSize";
-import SVGMaskAnimation from "@/components/Hero/SVGMaskAnimation";
-import Stats from "@/components/Hero/Stats";
+import React from 'react'
 
-export default function Home() {
+const page = () => {
   return (
-    <div className="bg-white text-black">
-      <HeroSection />
-      <LogoCarousel />
-      <MoreBorder />
-      <Ripple />
-      <SVGMaskAnimation />
-      <ValueProp />
-      <Stats />
-      <MarketSize />
-      <OurProses />
-      <Footer />
-    </div>
-  );
+    <div>page</div>
+  )
 }
+
+export default page
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,27 @@
-
-
-import LoadingHero from "@/components/LoadingHero";
-import Image from "next/image";
+import HeroSection from "@/components/Hero/HeroSection";
+import Ripple from "@/components/Hero/Ripple";
+import MoreBorder from "@/components/Hero/MoreBorder";
+import LogoCarousel from "@/components/Hero/LogoCarousel";
+import ValueProp from "@/components/Hero/ValueProp";
+import OurProses from "@/components/Hero/OurProses";
+import Footer from "@/components/Footer";
+import MarketSize from "@/components/Hero/MarketSize";
+import SVGMaskAnimation from "@/components/Hero/SVGMaskAnimation";
+import Stats from "@/components/Hero/Stats";
 
 export default function Home() {
   return (
-    <div className="">
-      <LoadingHero />
+    <div className="bg-white text-black">
+      <HeroSection />
+      <LogoCarousel />
+      <MoreBorder />
+      <Ripple />
+      <SVGMaskAnimation />
+      <ValueProp />
+      <Stats />
+      <MarketSize />
+      <OurProses />
+      <Footer />
     </div>
   );
 }
