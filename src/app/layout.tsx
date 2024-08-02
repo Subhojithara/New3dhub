89b0208fd90c68ff9012b8b20d99c25ee2520553
subhajit hara <harasubhojit@gmail.com>
2024-08-02 16:03:48 +0530

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from '@vercel/analytics/react';
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
@@ -25,6 +26,7 @@ export default function RootLayout({
         <Cursor />
         {children}
         <SpeedInsights />
+        <Analytics />
         </body>
     </html>
   );
